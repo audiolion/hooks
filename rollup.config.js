bc58990c48d6d6838cc63311732e2b9c3d459c17
Ryan Castner <castner.rr@gmail.com>
2019-07-17 14:17:44 -0400

    Remove terser, fixes for rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
-import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 const globals = { react: 'React' };
@@ -23,7 +22,6 @@ export default [
       typescript({
         typescript: require('typescript'),
       }), // so Rollup can convert TypeScript to JavaScript
-      terser(),
     ],
   },
 
@@ -40,7 +38,6 @@ export default [
       typescript({
         typescript: require('typescript'),
       }), // so Rollup can convert TypeScript to JavaScript,
-      terser(),
     ],
     output: [
       { file: pkg.main, format: 'cjs', globals },
